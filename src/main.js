de6660b5d38462a9047ce4417b12434b6dfdabb3
Bonface Njuguna <bonfacenjuguna13@gmail.com>
2022-07-22 23:34:19 +0300

    added incomponent route guards
    also updated db.json

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
-import { createApp } from 'vue'
+import { createApp, reactive } from 'vue' // <--- importing reactive
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'nprogress/nprogress.css'
 
-createApp(App).use(store).use(router).mount('#app')
+// Create a reactive object
+const GStore = reactive({ flashMessage: '' })
+
+createApp(App)
+  .use(store)
+  .use(router)
+  .provide('GStore', GStore) // provide this object so others can inject it
+  .mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import EventList from '../views/EventList.vue'
-import EventDetails from '@/views/EventDetails.vue'
+import EventLayout from '@/views/event/EventLayout.vue'
+import EventDetails from '@/views/event/EventDetails.vue'
+import EventRegister from '@/views/event/EventRegister.vue'
+import EventEdit from '@/views/event/EventEdit.vue'
+import NotFound from '@/views/NotFound.vue'
+import NetworkError from '@/views/NetworkError.vue'
 import About from '../views/MyAbout.vue'
 
 const routes = [
@@ -11,15 +16,54 @@ const routes = [
     props: (route) => ({ page: parseInt(route.query.page) || 1 }),
   },
   {
-    path: '/event/:id',
-    name: 'EventDetails',
+    path: '/events/:id',
+    name: 'EventLayout',
     props: true,
-    component: EventDetails,
+    component: EventLayout,
+    children: [
+      {
+        path: '',
+        name: 'EventDetails',
+        component: EventDetails,
+      },
+      {
+        path: 'register',
+        name: 'EventRegister',
+        component: EventRegister,
+      },
+      {
+        path: 'edit',
+        name: 'EventEdit',
+        component: EventEdit,
+      },
+    ],
   },
   {
-    path: '/about',
+    path: '/event/:afterEvent(.*)',
+    redirect: (to) => {
+      return { path: '/events/' + to.params.afterEvent }
+    },
+  },
+  {
+    path: '/about-us',
     name: 'About',
     component: About,
+  },
+  {
+    path: '/:catchAll(.*)',
+    name: 'NotFound',
+    component: NotFound,
+  },
+  {
+    path: '/404/:resource',
+    name: '404Resource',
+    component: NotFound,
+    props: true,
+  },
+  {
+    path: '/network-error',
+    name: 'NetworkError',
+    component: NetworkError,
   },
 ]
 
